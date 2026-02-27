2b605acfdfb8fe74d4c6da95c6cafe9f8fd38bb2
drgzkr <60198204+drgzkr@users.noreply.github.com>
2026-02-27 14:40:55 +0100

    Update remodnav_frame_selection.py
    config and paths

--- remodnav_frame_selection.py
+++ remodnav_frame_selection.py
@@ -3,17 +3,16 @@ import csv
 import pandas as pd
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+from config import REMODNAV_DIR, FIXATIONS_DIR
 
-save_path = '/path/Frames_Per_Sub_Remodnav/'
-
-sub_list = []
-
+sub_list = ['sub-01','sub-02','sub-03','sub-04','sub-09','sub-10','sub-14','sub-15','sub-16','sub-17','sub-18','sub-19','sub-20']
+frames_set = set()
 
 for run in tqdm(['1','2','3','4','5','6','7','8']):
     
     for sub in sub_list:
         
-        tsv_file = '/path/Remodnav/'+sub+'-run'+run+'.tsv'
+        tsv_file = os.path.join(REMODNAV_DIR, sub+'-run'+run+'.tsv')
         
         #open tsv file in pandas dataframe
         with open(tsv_file, newline='') as f:
@@ -36,10 +35,10 @@ for run in tqdm(['1','2','3','4','5','6','7','8']):
         fixation_distilled = np.array(fixation_distilled)
         fixation_frames = fixation_distilled[:,0]
 
-        np.save(save_path+sub+'_frames_used_in_sec_run_'+run,fixation_distilled)
+        np.save(os.path.join(FIXATIONS_DIR, sub+'-run'+run+'.npy'), fixation_distilled)
         
         for fix in fixation_frames:
             frames_set.add(fix)
             
     frames_set = np.array(list(frames_set)).astype(int)
-    np.save(save_path+'Needed_frames_Run_'+run,frames_set)
+    np.save(os.path.join(FIXATIONS_DIR, 'Needed_frames_Run_'+run+'.npy'), frames_set)
