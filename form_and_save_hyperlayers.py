25209dc904a488aa5b1b73dea8e7e3108b1fefc1
drgzkr <60198204+drgzkr@users.noreply.github.com>
2026-02-27 14:25:36 +0100

    Update form_and_save_hyperlayers.py
    config and paths

--- form_and_save_hyperlayers.py
+++ form_and_save_hyperlayers.py
@@ -7,19 +7,20 @@ Created on Tue Nov 29 13:17:45 2022
 """
 import numpy as np
 from tqdm import tqdm
+from config import VGG_BY_SUB_DIR, HYPERLAYERS_DIR
 
 sub_list = ['sub-01','sub-02','sub-03','sub-04','sub-09','sub-10','sub-14','sub-15','sub-16','sub-17','sub-18','sub-19','sub-20']
 
 for sub in tqdm(sub_list):
     
-    sub_features_shape = np.load('/home/djaoet/wrkgrp/Dora/Study_Forrest/vgg19/FeaturesBySubsRemodnav/Model_Features_'+sub+'_concat_run_layer0_7_16.npy').shape
+    sub_features_shape = sub_features_shape = np.load(os.path.join(VGG_BY_SUB_DIR, 'Model_Features_'+sub+'_concat_run_layer0_7_16.npy')).shape
     sub_feature_sizes = np.array([64,128,256,512,512])
     zero_sub_feature_sizes = np.insert(np.cumsum(sub_feature_sizes),0,0)
     
     sub_hyperlayer_features = np.zeros((sub_features_shape[0],np.sum(sub_feature_sizes),7,16))
     
     for layer in ['0','1','2','3','4']:
-        sub_features = np.load('/home/djaoet/wrkgrp/Dora/Study_Forrest/vgg19/FeaturesBySubsRemodnav/Model_Features_'+sub+'_concat_run_layer'+layer+'_7_16.npy')
+        sub_features = np.load(os.path.join(VGG_BY_SUB_DIR, 'Model_Features_'+sub+'_concat_run_layer'+layer+'_7_16.npy'))
         sub_hyperlayer_features[:,zero_sub_feature_sizes[int(layer)]:zero_sub_feature_sizes[int(layer)+1],:,:] = sub_features
     
-    np.save('/home/djaoet/wrkgrp/Dora/Study_Forrest/vgg19/HyperlayersBySubsRemodnav/Model_features_'+sub+'_hyperlayer_7_16',sub_hyperlayer_features,allow_pickle=True)
+    np.save(os.path.join(HYPERLAYERS_DIR, 'Model_features_'+sub+'_hyperlayer_7_16.npy'), sub_hyperlayer_features, allow_pickle=True)
