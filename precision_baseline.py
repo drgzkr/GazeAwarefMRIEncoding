2553284d578666bb9f96901b4d6f33e25ca25c19
drgzkr <60198204+drgzkr@users.noreply.github.com>
2026-02-27 14:33:11 +0100

    Update precision_baseline.py
    config and paths

--- precision_baseline.py
+++ precision_baseline.py
@@ -7,6 +7,7 @@ import warnings
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
+from config import FIXATIONS_DIR, HYPERLAYERS_DIR, FMRI_DIR, RESULTS_DIR
 
 warnings.filterwarnings('ignore')
 
@@ -113,10 +114,10 @@ for sub in tqdm(sub_list,position=0):
 
     # x and y sizes of feature maps to be used. Can change each flexibly
     print('features are being loaded ⏳')
-	sub_features = np.load('/home/djaoet/wrkgrp/Dora/Study_Forrest/vgg19/HyperlayersBySubsRemodnav/Model_features_'+sub+'_hyperlayer_7_16.npy',allow_pickle=True)
+	sub_features = np.load(os.path.join(HYPERLAYERS_DIR, 'Model_features_'+sub+'_hyperlayer_7_16.npy'), allow_pickle=True)
     print('features loaded 👌')
     # load brain data
-    sub_movie_data = np.load('/home/djaoet/wrkgrp/Dora/Study_Forrest/Movie_Data/normalized_masked_concat_movie_'+sub+'.npy',allow_pickle=True)
+    sub_movie_data = np.load(os.path.join(FMRI_DIR, 'normalized_masked_concat_movie_'+sub+'.npy'), allow_pickle=True)
     print('fMRI data loaded 👌')
     # load concatenated fixation frames and coordinates, and collect some info about runs
     concat_fixations = np.zeros((sub_features.shape[0],3))
@@ -125,7 +126,7 @@ for sub in tqdm(sub_list,position=0):
     counter = 0
 
     for run in ['1','2','3','4','5','6','7','8']:
-        run_fixations = np.load('/home/djaoet/wrkgrp/Dora/Study_Forrest/Rect Alexnet/Frames_Per_Sub_Remodnav/Needed_frames_'+sub+'-run'+run+'.npy',allow_pickle=True)
+        run_fixations = np.load(os.path.join(FIXATIONS_DIR, sub+'-run'+run+'.npy'), allow_pickle=True)
         concat_fixations[counter:counter+run_fixations.shape[0]] = run_fixations
         run_fix_lens.append(run_fixations.shape[0])
         run_sub_frames.append(run_fixations[:,0].astype(int))
@@ -262,14 +263,15 @@ for sub in tqdm(sub_list,position=0):
     gammas.append(best_gamma)
     
     learned_ws = fastRidge.get_weights()
-    np.save('/home/djaoet/wrkgrp/Dora/Study_Forrest/FastBaselineResults/'+str(sub)+'_hyperlayer_fast_7gammae7_baseline_trained_feature_weights',learned_ws,allow_pickle=True)
+    np.save(os.path.join(RESULTS_DIR, str(sub)+'_hyperlayer_fast_baseline_weights.npy'), learned_ws, allow_pickle=True)
 
     print('weights shape: ',learned_ws.shape)
     for voxel_count, (predicted, real) in enumerate(zip(y_test_hat.T,x_te.T)):
         if ~np.isnan(predicted).any():
             results[voxel_count,0], results[voxel_count,1] = pearsonr(predicted,real)
     print('Mean Performance: ',np.mean(results[:,0]))
-    np.save('/home/djaoet/wrkgrp/Dora/Study_Forrest/FastBaselineResults/'+str(sub)+'_hyperlayer_fast_baseline_results',results,allow_pickle=True)
-np.save('/home/djaoet/wrkgrp/Dora/Study_Forrest/FastBaselineResults/'+str(sub)+'_hyperlayer_fast_baseline_gammas',gammas,allow_pickle=True)
+    np.save(os.path.join(RESULTS_DIR, str(sub)+'_hyperlayer_fast_baseline_results.npy'), results, allow_pickle=True)
+np.save(os.path.join(RESULTS_DIR, str(sub)+'_hyperlayer_fast_baseline_gammas.npy'), gammas, allow_pickle=True)
+
 
 print('results saved ✨')
