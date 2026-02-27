e3d4dbac67f37493e1fe47b581044f127e9e17a0
drgzkr <60198204+drgzkr@users.noreply.github.com>
2026-02-27 15:01:17 +0100

    Update remodnav_frame_selection.py

--- remodnav_frame_selection.py
+++ remodnav_frame_selection.py
@@ -7,7 +7,6 @@ from config import REMODNAV_DIR, FIXATIONS_DIR
 import os
 
 sub_list = ['sub-01','sub-02','sub-03','sub-04','sub-09','sub-10','sub-14','sub-15','sub-16','sub-17','sub-18','sub-19','sub-20']
-frames_set = set()
 
 for run in tqdm(['1','2','3','4','5','6','7','8']):
     
